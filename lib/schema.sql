1ce3900481a0408e9ff81b272a3f623dbcbe577b
Matthew C <m23.cullen@gmail.com>
2019-10-12 17:39:39 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,5 +1,6 @@
-CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT);
+CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, sub-genre_id INTEGER);
 CREATE TABLE sub-genres (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
-CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER);
-CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT,);
+CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
+CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER);
+CREATE TABLE character_books (if INTEGER PRIMARY KEY, character_id INTEGER, book_id INTEGER)
