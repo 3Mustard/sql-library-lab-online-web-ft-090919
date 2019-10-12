3e0000f3d1d58f54477aa25f10833576350b629f
Matthew C <m23.cullen@gmail.com>
2019-10-12 17:35:16 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,2 +1,5 @@
-CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT)
-CREATE TABLE sub-genres (id )
+CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT);
+CREATE TABLE sub-genres (id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER);
+CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT,);
