7c47cc825cf8f11da7b9c365ce17e043ee2a8919
Matthew C <m23.cullen@gmail.com>
2019-10-12 17:56:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,18 +4,20 @@ INSERT INTO Series (title, author_id, subgenre_id) VALUES ("Old Kingdom", 2, 2);
 INSERT INTO Books (title, year, series_id) VALUES ("The Eye of the World", 1990, 1);
 INSERT INTO Books (title, year, series_id) VALUES ("The Great Hunt", 1990, 1);
 INSERT INTO Books (title, year, series_id) VALUES ("The Dragon Reborn", 1991, 1);
+
 INSERT INTO Books (title, year, series_id) VALUES ("Sabriel", 1995, 2);
 INSERT INTO Books (title, year, series_id) VALUES ("Liriel", 1996, 2);
 INSERT INTO Books (title, year, series_id) VALUES ("Abhorsen", 1998, 2);
 
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Galadriel", "This is Nenya, the Ring of Adamant, and I am its keeper.", "elf", 1, 1);
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Legolas", "Do not think I won't kill you, dwarf! It would be my pleasure", "elf", 1, 1);
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Frodo Baggins", "I will take the Ring, though I do not know the way...", "hobbit", 1, 1);
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Harry Potter", "Expeliamus!", "human", 1, 1);
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Hermione Granger", "You're going to get yourself killed...", "human", 2, 2);
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Ron Weasley", "I've got a bad feeling about this...", "human", 2, 2);
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Lucius Malfoy", "Die Potter!", "human", 2, 2);
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Rubeus Hagrid", "Just gotta know how to calm 'em", "half-giant", 2, 2);
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("", "", "", 1, 1);
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("", "", "", 1, 1);
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("", "", "", 1, 1);
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("", "", "", 1, 1);
+
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("", "", "", 2, 2);
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("", "", "", 2, 2);
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("", "", "", 2, 2);
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("", "", "", 2, 2);
 
 INSERT INTO SubGenres (name) VALUES ("Wizardry");
 INSERT INTO SubGenres (name) VALUES ("Medieval Lore");
